64d30da2334a7a109ea58d1a8c1147992601c974
a <st142005@m01.kyoto-kcg.ac.jp>
2022-01-20 10:11:58 +0900

    01/20 名称変更・削除

---
+++ login.php
@@ -0,0 +1,55 @@
+<?php
+$error = "";
+if(!empty($_GET['error'])){
+    switch($_GET["error"]){
+        case "e1":
+            $error = "そのユーザーIDは存在しません。";
+            break;
+        case "e2":
+            $error = "パスワードに誤りがあります。";
+            break;
+    }
+}
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ログイン</title>
+        <link rel="stylesheet" href="style.css" media="all" />
+        <link rel="stylesheet" href="koumoku.css">
+    </head>
+<body>
+    <header>
+        <nav>
+            <ul class="clearfix">
+                <li><a href="index.php">一覧画面</a></li>
+                <li><a href="post.php">投稿画面</a></li>
+                <li><a href="Logout.php">ログアウト</a></li>
+            </ul>
+        </nav>
+    </header>
+    <?php if(!empty($_GET['login'])){print("<p>ログインしてください</p>");} ?>
+    <div class ="form-wrapper">
+    	<form id="loginForm" name="loginForm" action="checkLogin.php" method="POST">
+			<fieldset>
+				<legend>ログイン</legend>
+            	<div><font color="#ff0000"><?php if(!empty($_GET['error'])){ echo htmlspecialchars($error, ENT_QUOTES);} ?></font></div>
+            	<label for="userid" class = "userid">ユーザーID　</label><input type="text" id="userid" name="userid" placeholder="ユーザーIDを入力" value=""required>
+            	<br>
+            	<label for="password" class = "pass">パスワード　</label><input type="password" id="password" name="password" value="" placeholder="パスワードを入力" required>
+            	<br>
+            	<input type="submit" id="login" name="login" value="ログイン">
+        	</fieldset>
+		</form>
+    </div>
+    <br>
+​
+    <form action="signUp.php">
+        <fieldset>
+            <legend>新規登録はこちら</legend>
+            <input type="submit" value="新規登録">
+        </fieldset>
+    </form>
+</body>
+</html>
