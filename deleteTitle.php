06f2e04ee8bcd404a862bc5c0c34b3e1de87598e
a <st142005@m01.kyoto-kcg.ac.jp>
2022-02-01 01:26:48 +0900

    01/31 変更

--- deleteTitle.php
+++ deleteTitle.php
@@ -2,7 +2,7 @@
 require_once ('functions.php');
 $dbh = connectDB();
 $dbh->exec("USE nksdb");
-$sql = ("DELETE FROM media WHERE id = '" .$_POST[id]."'");
+$sql = ("DELETE FROM media WHERE id = '" .$_POST["id"]."'");
 $prepare = $dbh->prepare($sql);
 $prepare->execute();
 $dbh = NULL;
